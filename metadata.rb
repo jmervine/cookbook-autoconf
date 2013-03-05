1388056a2a14cc38bf2e445118660dae61c503ff
Josh Mervine <jmervine@yp.com>
2013-03-05 14:52:35 -0800

    updating metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
 name             "autoconf"
 maintainer       "Joshua Mervine"
 maintainer_email "joshua@mervine.net"
-license          "All rights reserved"
+license          "MIT"
 description      "Installs/Configures autoconf"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+recipe           "autoconf::default", "Builds autoconf from source."
 version          "0.1.0"
 depends          "build-essential"
 
